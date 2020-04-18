a64524f026ec4cd23c75d9e21de9313ba9bcadae
Eliseev Eugene <eliseevmail@ya.ru>
2020-04-18 19:02:52 +0300

    Add Command

--- PasteProperty/PastePropertyPackage.cs
+++ PasteProperty/PastePropertyPackage.cs
@@ -25,6 +25,7 @@ namespace PasteProperty
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(PastePropertyPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class PastePropertyPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,9 @@ namespace PasteProperty
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ConvertSelectedToPropertyCommand.InitializeAsync(this);
+            await ConvertSelectedToFieldCommand.InitializeAsync(this);
+            await ConvertSelectedToPrivateFieldCommand.InitializeAsync(this);
         }
 
         #endregion
